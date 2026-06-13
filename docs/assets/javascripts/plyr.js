ebce38d11aa81a4d46e88ece13f1319c1420e07f
DessertSource <48232628+DessertSource@users.noreply.github.com>
2026-06-14 00:18:26 +0700

    Media: Set default video quality in plyr to 1080p instead of 540p

--- docs/assets/javascripts/plyr.js
+++ docs/assets/javascripts/plyr.js
@@ -7,7 +7,7 @@ document$.subscribe(function() {
         controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'fullscreen'],
         settings: ['quality', 'speed'],
         quality: {
-          default: 540,
+          default: 1080,
           options: [1080, 540]
         }
       });
