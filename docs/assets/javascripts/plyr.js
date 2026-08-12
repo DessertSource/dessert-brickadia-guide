e9781de5b5010530063ba6086a97965a86972900
DessertSource <dessertsource@noreply.codeberg.org>
2026-08-12 09:30:32 +0700

    Scripts: Attempt to fix plyr.js svg path

--- docs/assets/javascripts/plyr.js
+++ docs/assets/javascripts/plyr.js
@@ -1,10 +1,15 @@
 document$.subscribe(function() {
   const elements = document.querySelectorAll('video');
   
+  // Initialize the relative path.
+  const scriptEl = document.currentScript ||
+    document.querySelector('script[src*="assets/javascripts/plyr.js"]')
+  const iconUrl = new URL('../stylesheets/elements/plyr.svg', scriptEl.src).href;
+
   elements.forEach(element => {
     if (!element.plyr) {
       new Plyr(element, {
-        iconUrl: '/assets/stylesheets/elements/plyr.svg',
+        iconUrl: iconUrl,
         controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'fullscreen'],
         settings: ['quality', 'speed'],
         quality: {
