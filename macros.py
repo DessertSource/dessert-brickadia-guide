b1f47d3a121f26b9545167e42269aae1a20c6369
DessertSource <dessertsource@noreply.codeberg.org>
2026-08-17 00:00:01 +0700

    Macros: Refactor variable display macro.

--- macros.py
+++ macros.py
@@ -1,104 +1,246 @@
 def define_env(env):
 
     # Variable type display
-    @env.macro
-    def var_display(value, data_type, is_reference = None) -> None:
-        
-        icon = ""
-        color = ""
-        text = ""
-        tooltip = ""
-        space = " "
-        reference_class = ""
-        reference_info = ""
 
-        if is_reference == "normal":
-            reference_class = " var-reference"
-            reference_info = "Variable reference: "
-        if is_reference == "array":
-            reference_class = " var-array-reference"
-            reference_info = "Array variable reference: "
+    @env.macro
+    def var_display(value, data_type, secondary_type = None, map_key_type = None) -> None:
+
+        # CSS classes
+        main_classes = " "
+        key_classes = "."
+
+        # Text/caption
+        main_text = " "
+        key_text = ""
+
+        # Icon
+        main_icon = ""
+
+        # Tooltip / HTML title of variable display span element
+        tooltip = ""
+
+        # Overwrite the string variables above based on data type given by the
+        # user. New types can be added as they come in subsequent updates.
 
         if data_type == "boolean":
-            icon = "lucide-check-square"
-            text = "Boolean"
-            tooltip = text
-            color = "red"
-        elif data_type == "integer":
-            icon = "lucide-hash"
-            text = "Integer"
-            tooltip = text
-            color = "teal"
-        elif data_type == "float":
-            icon = "lucide-spline"
-            text = "Float"
-            tooltip = text
-            color = "green"
-        elif data_type == "string":
-            icon = "lucide-type"
-            text = "String"
-            tooltip = text
-            color = "orange"
-        elif data_type == "vector":
-            icon = "lucide-axis-3d"
-            text = "Vector"
-            tooltip = text
-            color = "yellow"
-        elif data_type == "rotation":
-            icon = "lucide-rotate-cw"
-            text = "Rotation"
-            tooltip = text
-            color = "lime"
-        elif data_type == "quaternion":
-            icon = "lucide-rotate-3d"
-            text = "Quaternion"
-            tooltip = text
-            color = "sky-blue"
-        elif data_type == "reference":
-            icon = "lucide-link"
-            text = "Reference"
-            tooltip = text
-            color = "cyan"
-        elif data_type == "color":
-            icon = "lucide-palette"
-            text = "Color"
-            tooltip = text
-            color = "pink"
-        elif data_type == "enum":
-            icon = "lucide-list-ordered"
-            text = "Enum"
-            tooltip = text
-            color = "lilac"
-        elif data_type == "exec":
-            icon = "lucide-split"
-            text = "Exec"
-            tooltip = text
-        elif data_type == "special":
-            icon = "lucide-boxes"
-            text = value
-            tooltip = text
-            color = "blue"
 
-        if value != None:
-            text = value
-            if data_type == "vector":
-                text = f'**X: {value[0]}**{{.array}} **Y: {value[1]}**{{.array}} **Z: {value[2]}**{{.array}}'
-            elif data_type == "rotation":
-                text = f'**Roll: {value[0]}&#176;**{{.array}} **Pitch: {value[1]}&#176;**{{.array}} **Yaw: {value[2]}&#176;**{{.array}}'
-            elif data_type == "quaternion":
-                text = f'**X: {value[0]}**{{.array}} **Y: {value[1]}**{{.array}} **Z: {value[2]}**{{.array}} **W: {value[3]}**{{.array}}'
-            elif data_type == "color":
-                text = f':material-square:{{style="color:rgb({value[0]},{value[1]},{value[2]});"}} **R: {value[0]}**{{.array}} **G: {value[1]}**{{.array}} **B: {value[2]}**{{.array}}'
+            main_classes += "red"
+            main_text += "Boolean"
+            main_icon = "lucide-check-square"
+            tooltip = "Boolean"
+
+            if value == False:
+                main_icon = "lucide-square"
+                main_text = " False"
+
+            elif value == True:
+                main_text = " True"
+
+            elif value == None:
+                pass
+
+            else:
+                main_text = " " + str(value)
+        
+        if data_type == "integer":
+
+            main_classes += "teal"
+            main_text += "Integer"
+            main_icon = "lucide-hash"
+            tooltip = "Integer"
+
+            if value == None:
+                pass
+
+            else:
+                main_text = " " + str(value)
+
+        if data_type == "float":
+            
+            main_classes += "green"
+            main_text += "Number/Float"
+            main_icon = "lucide-spline"
+            tooltip = "Number/Float"
+
+            if value == None:
+                pass
+
+            else:
+                main_text = " " + str(value)
+
+        if data_type == "string":
+
+            main_classes += "orange"
+            main_text += "String"
+            main_icon = "lucide-type"
+            tooltip = "String"
+
+            if value == None:
+                pass
+
+            else:
+                main_text = " " + str(value)
+
+        if data_type == "vector":
+
+            main_classes += "yellow"
+            main_text += "Vector"
+            main_icon = "lucide-axis-3d"
+            tooltip = "Vector"
+
+            if value != None:
+                if len(value) == 2:
+                    main_text = f' **X: {value[0]}**{{.array}} **Y: {value[1]}**{{.array}}'
+                elif len(value) == 3:
+                    main_text = f' **X: {value[0]}**{{.array}} **Y: {value[1]}**{{.array}} **Z: {value[2]}**{{.array}}'
+                else:
+                    main_text = " " + str(value)
+            elif value == None:
+                pass
+
+        if data_type == "rotation":
+
+            main_classes += "lime"
+            main_text += "Rotation"
+            main_icon = "lucide-rotate-cw"
+            tooltip = "Rotation"
+
+            if value != None:
+                if len(value) == 3:
+                    main_text = f' **R: {value[0]}**{{.array}} **P: {value[1]}**{{.array}} **Y: {value[2]}**{{.array}}'
+                else:
+                    main_text = " " + str(value)
+            elif value == None:
+                pass
+
+        if data_type == "quaternion":
+
+            main_classes += "sky-blue"
+            main_text += "Quaternion"
+            main_icon = "lucide-rotate-3d"
+            tooltip = "Quaternion"
+
+            if value != None:
                 if len(value) == 4:
-                    text += f' **A: {value[3]}**{{.array}}'
-            elif data_type == "enum":
-                text = f'**{value[0]}**{{.array}} = **{value[1]}**'
-            elif value == "False":
-                icon = "lucide-square"
-        if value == "":
-            space = "";
-        return f'<span class="text info-mini {color}{reference_class}" title="{reference_info}{tooltip}" markdown><b> :{icon}:{{.trans}}{space}{text}</b></span>'
+                    main_text = f' **X: {value[0]}**{{.array}} **Y: {value[1]}**{{.array}} **Z: {value[2]}**{{.array}} **W: {value[3]}**{{.array}}'
+                else:
+                    main_text = " " + str(value)
+            elif value == None:
+                pass
 
+        if data_type == "object":
+            
+            main_classes += "cyan"
+            main_text += "Object/Reference"
+            main_icon = "lucide-link"
+            tooltip = "Object/Reference"
+
+            if value == None:
+                pass
+
+            else:
+                main_text = " " + str(value)
+
+        if data_type == "color":
+
+            main_classes += "pink"
+            main_text += "Color"
+            main_icon = "lucide-palette"
+            tooltip = "Color"
+
+            if value != None:
+                if len(value) == 3:
+                    main_text = f' :material-square:{{style="color:rgb({value[0]},{value[1]},{value[2]});"}} **R: {value[0]}**{{.array}} **G: {value[1]}**{{.array}} **B: {value[2]}**{{.array}}'
+                elif len(value) == 4:
+                    main_text = f' :material-square:{{style="color:rgb({value[0]},{value[1]},{value[2]});"}} **R: {value[0]}**{{.array}} **G: {value[1]}**{{.array}} **B: {value[2]}**{{.array}} **A: {value[3]}**{{.array}}'
+                else:
+                    main_text = " " + str(value)
+            elif value == None:
+                pass
+
+        if data_type == "enum":
+
+            main_classes += "lilac"
+            main_text += "Enum"
+            main_icon = "lucide-list-ordered"
+            tooltip = "Enum"
+
+            if value != None:
+                if len(value) == 2:
+                    main_text = f' **{value[0]}**{{.array}} = **{value[1]}**'
+                else:
+                    main_text = " " + str(value)
+            elif value == None:
+                pass
+
+        if data_type == "exec":
+
+            main_text += "Exec"
+            main_icon = "lucide-split"
+            tooltip = "Exec"
+
+            if value == None:
+                pass
+
+            else:
+                main_text = " " + str(value)
+
+        if data_type == "special":
+            
+            main_classes += "blue"
+            main_text += str(value)
+            main_icon = "lucide-boxes"
+            tooltip = str(value)
+
+        # Clear the space if a color is not specified.
+        if main_classes == " ":
+            main_classes = ""
+
+        # Build text for secondary class
+        if secondary_type == "var-ref":
+            main_classes += " var-ref"
+            tooltip = "Variable reference: " + tooltip
+        elif secondary_type == "array-ref":
+            main_classes += " array-ref"
+            tooltip = "Array variable reference: " + tooltip
+        elif secondary_type == "map-ref":
+            main_classes += " map-ref"
+            tooltip = "Map array variable reference: " + tooltip
+
+            # Only execute this if the secondary type is specified as
+            # map-ref (map array variable reference).
+            if map_key_type == "integer":
+                main_classes += " key-integer"
+                key_text = "Int."
+                tooltip += " (Integer keys)"
+            elif map_key_type == "string":
+                main_classes += " key-string"
+                key_text = "Str."
+                tooltip += " (String keys)"
+            elif map_key_type == "object":
+                main_classes += " key-object"
+                key_text = "Obj."
+                tooltip += " (Object keys)"
+
+        # Clear the space if the user gives an empty string.
+        if value == "":
+            main_text = ""
+
+        # Build output: Main classes, tooltip, icon and text.
+        output = f'<span class="text info-mini {main_classes}" title="{tooltip}" markdown><b>:{main_icon}:{{.trans}}{main_text}'
+
+        # Build additional output as needed.
+        if secondary_type == "var-ref":
+            output += f' **[V]**{{.text .blue}}'
+        elif secondary_type == "array-ref":
+            output += f' **[Av]**{{.text .blue}}'
+        if secondary_type == "map-ref":
+            output += f' **[Mv: {key_text}]**{{.text .blue}}'
+
+        output += f'</b></span>'
+
+        return output
+    
     # Gate availability
     @env.macro
     def gate_availability(value, is_advanced = False) -> None:
